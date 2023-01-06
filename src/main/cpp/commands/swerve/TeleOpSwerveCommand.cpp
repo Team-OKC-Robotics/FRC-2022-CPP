71159af8f94715ea71abfcdad0a2972c7584dd79
danielbrownmsm <daniel.brown@msmrockets.org>
2023-01-05 18:44:07 -0600

    remove tele-op input cubing, add strafe_power and fix it

--- src/main/cpp/commands/swerve/TeleOpSwerveCommand.cpp
+++ src/main/cpp/commands/swerve/TeleOpSwerveCommand.cpp
@@ -2,7 +2,6 @@
 #include "commands/swerve/TeleOpSwerveCommand.h"
 
 TeleOpSwerveCommand::TeleOpSwerveCommand(std::shared_ptr<SwerveDrive> swerve, std::shared_ptr<frc::Joystick> gamepad) {
-    // Set everything.
     swerve_ = swerve;
     gamepad_ = gamepad;
 
@@ -19,12 +18,11 @@ void TeleOpSwerveCommand::Execute() {
     VOKC_CHECK(swerve_ != nullptr);
     VOKC_CHECK(gamepad_ != nullptr);
 
-    double throttle = -1.0 * pow(gamepad_->GetRawAxis(1), 3);
-    double turn_power = pow(gamepad_->GetRawAxis(4), 3);
+    double drive_power = -this->gamepad_->GetRawAxis(1);
+    double strafe_power = this->gamepad_->GetRawAxis(2);
+    double turn_power = this->gamepad_->GetRawAxis(3);
 
-    // TODO actually get correct joystick inputs
-
-    VOKC_CALL(swerve_->TeleOpDrive(throttle, turn_power));
+    VOKC_CALL(swerve_->TeleOpDrive(drive_power, strafe_power, turn_power));
 }
 
 bool TeleOpSwerveCommand::IsFinished() {
